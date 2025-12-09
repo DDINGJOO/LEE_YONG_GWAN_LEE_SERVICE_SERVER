cf1c01cd6dde9e60a886d85ec9ab03fc75fcca9b
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-09 15:49:49 +0900

    [CHORE] Apply code formatting to TestRedisConfig

--- springProject/src/test/java/com/teambind/springproject/config/TestRedisConfig.java
+++ springProject/src/test/java/com/teambind/springproject/config/TestRedisConfig.java
@@ -4,21 +4,19 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import static org.mockito.Mockito.mock;
 
 /**
  * 테스트용 Redis 설정.
- *
+ * <p>
  * Redis 관련 빈들을 Mock으로 제공한다.
  * ShedLock은 TestShedLockConfig에서 H2 기반으로 처리한다.
  */
 @TestConfiguration
 public class TestRedisConfig {
-
+	
 	@Bean
 	@Primary
 	public RedisConnectionFactory redisConnectionFactory() {
@@ -26,7 +24,7 @@ public class TestRedisConfig {
 		// 실제 연결이 필요한 경우 TestContainers 사용 권장
 		return mock(RedisConnectionFactory.class);
 	}
-
+	
 	@Bean
 	@Primary
 	public StringRedisTemplate stringRedisTemplate() {
